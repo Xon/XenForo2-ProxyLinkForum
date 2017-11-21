8fdb6110e31c3bdb7325449369f1e9de2277f323
Xon <tazipper1@hotmail.com>
2017-11-21 10:24:41 +0800

    Simplify add-on

--- upload/src/addons/SV/ProxyLinkForum/XF/Admin/Controller/LinkForum.php
+++ upload/src/addons/SV/ProxyLinkForum/XF/Admin/Controller/LinkForum.php
@@ -6,11 +6,11 @@ use XF\Mvc\FormAction;
 
 class LinkForum extends XFCP_LinkForum
 {
-	protected function saveTypeData(FormAction $form, \XF\Entity\Node $node, \XF\Entity\AbstractNode $data)
-	{
-		parent::saveTypeData($form, $node, $data);
+    protected function saveTypeData(FormAction $form, \XF\Entity\Node $node, \XF\Entity\AbstractNode $data)
+    {
+        parent::saveTypeData($form, $node, $data);
 
-		$proxyNodeId = $this->filter('sv_proxy_node_id', 'uint');
-		$data->set('sv_proxy_node_id', $proxyNodeId);
-	}
+        $proxyNodeId = $this->filter('sv_proxy_node_id', 'uint');
+        $data->set('sv_proxy_node_id', $proxyNodeId);
+    }
 }
--- upload/src/addons/SV/ProxyLinkForum/XF/Entity/LinkForum.php
+++ upload/src/addons/SV/ProxyLinkForum/XF/Entity/LinkForum.php
@@ -2,31 +2,67 @@
 
 namespace SV\ProxyLinkForum\XF\Entity;
 
+use XF\Mvc\Entity\Entity;
+use XF\Mvc\Entity\Structure;
+
 /**
  * @property \XF\Entity\Forum ProxiedForum
+ * @property int|null sv_proxy_node_id
  */
 class LinkForum extends XFCP_LinkForum
 {
-	public function getNodeListExtras()
-	{
-		if ($this->ProxiedForum)
-		{
-			return $this->ProxiedForum->getNodeListExtras();
-		}
+    public function getNodeListExtras()
+    {
+        if ($this->ProxiedForum)
+        {
+            return $this->ProxiedForum->getNodeListExtras();
+        }
 
-		return parent::getNodeListExtras();
-	}
+        return parent::getNodeListExtras();
+    }
 
-	public function getNodeTemplateRenderer($depth)
-	{
-		if (!$this->ProxiedForum)
-		{
-			return parent::getNodeTemplateRenderer($depth);
-		}
+    public function getNodeTemplateRenderer($depth)
+    {
+        if (!$this->ProxiedForum)
+        {
+            return parent::getNodeTemplateRenderer($depth);
+        }
 
-		return [
-			'template' => 'node_list_forum',
-			'macro' => $depth <= 2 ? 'depth' . $depth : 'depthN'
-		];
-	}
+        return [
+            'template' => 'node_list_forum',
+            'macro'    => $depth <= 2 ? 'depth' . $depth : 'depthN'
+        ];
+    }
+
+    public function _preSave()
+    {
+        if ($this->sv_proxy_node_id && !$this->link_url)
+        {
+            $this->link_url = $this->app()->router('public')->buildLink('canonical:forums', $this->ProxiedForum);
+        }
+        parent::_preSave();
+    }
+
+    public static function getStructure(Structure $structure)
+    {
+        $structure = parent::getStructure($structure);
+
+        $structure->columns['sv_proxy_node_id'] = [
+            'type'     => Entity::UINT,
+            'nullable' => true,
+            'default'  => null
+        ];
+
+        $structure->relations['ProxiedForum'] = [
+            'entity'      => 'XF:Forum',
+            'type'        => Entity::TO_ONE,
+            'conditions'  => [['node_id', '=', '$sv_proxy_node_id']],
+            'defaultWith' => 'node',
+            'primary'     => true
+        ];
+
+        $structure->defaultWith[] = 'ProxiedForum';
+
+        return $structure;
+    }
 }
