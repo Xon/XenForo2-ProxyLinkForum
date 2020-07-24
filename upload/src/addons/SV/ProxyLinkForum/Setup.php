05188114229e4b45b3b7526e55581a5cd4da51af
Xon <tazipper1@hotmail.com>
2020-07-24 16:37:10 +0800

    version dependency bump and depend on new standard lib add-on

--- upload/src/addons/SV/ProxyLinkForum/Setup.php
+++ upload/src/addons/SV/ProxyLinkForum/Setup.php
@@ -2,7 +2,7 @@
 
 namespace SV\ProxyLinkForum;
 
-use SV\Utils\InstallerHelper;
+use SV\StandardLib\InstallerHelper;
 use XF\AddOn\AbstractSetup;
 use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
@@ -11,7 +11,6 @@ use XF\Db\Schema\Alter;
 
 class Setup extends AbstractSetup
 {
-    // from https://github.com/Xon/XenForo2-Utils cloned to src/addons/SV/Utils
     use InstallerHelper;
     use StepRunnerInstallTrait;
     use StepRunnerUpgradeTrait;
