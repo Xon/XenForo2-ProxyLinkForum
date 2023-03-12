94c2cfce405fec8c1ef70e39eff2210d57a12d1d
Xon <635541+Xon@users.noreply.github.com>
2023-03-12 14:08:51 +0800

    Add isProxiedNode helper method

--- upload/src/addons/SV/ProxyLinkForum/XF/Entity/Node.php
+++ upload/src/addons/SV/ProxyLinkForum/XF/Entity/Node.php
@@ -9,6 +9,21 @@ use XF\Entity\AbstractNode;
  */
 class Node extends XFCP_Node
 {
+    public function isProxiedNode(): bool
+    {
+        if ($this->node_type_id === 'LinkForum')
+        {
+            /** @var ?LinkForum $linkedForum */
+            $linkedForum = $this->Data;
+            if ($linkedForum !== null && $linkedForum->sv_proxy_node_id !== null)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public function setData(?AbstractNode $data): void
     {
         $this->_getterCache['Data'] = $data;
